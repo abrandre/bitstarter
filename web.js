d105ab2f93820c88935836f4c26e2e41632d2be2
abrandre <abrandre@gmail.com>
2013-07-06 07:41:03 +0000

    modified web.js

--- web.js
+++ web.js
@@ -3,11 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    
+
     var fs = require('fs');
-    var buffer = new buffer();
-    
-    response.send(buffer.toString('utc 8', fs.readFileSync("index.html")));
+    var buf = new buffer();
+    var txt = buf.toString(fs.readFileSync('index.html'));
+    response.send('txt');
 
     //response.send('Hello World2!');
 
@@ -17,4 +17,3 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
