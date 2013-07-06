722753dade57f5fdb4ac05fda526ea49d313d333
abrandre <abrandre@gmail.com>
2013-07-06 05:06:32 +0000

    back to hello again

--- web.js
+++ web.js
@@ -4,15 +4,15 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     
-    var fs = require('fs');
-    var buffer = new buffer();
+    //var fs = require('fs');
+    //var buffer = new buffer();
 	
     //fs.readFileSync('index.html');
     //buf.toString(fs); 
     
-    response.send(buffer.toString('utc 8', fs.readFileSync("index.html")));
+    //response.send(buffer.toString('utc 8', fs.readFileSync("index.html")));
 
-    //response.send('Hello World2!');
+    response.send('Hello World2!');
 
 });
 
