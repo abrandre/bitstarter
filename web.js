d79e0ce887ccf8889b33280ec5234810a441d5f6
abrandre <abrandre@gmail.com>
2013-07-06 05:00:05 +0000

    get it back

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
     
     //var fs = require('fs');
-    //var buffer = new Buffer();
+    //var buffer = new buffer();
 	
     //fs.readFileSync('index.html');
     //buf.toString(fs); 
