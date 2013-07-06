36be6618b1acc1b5d1b236ac5e0e465c7207853a
abrandre <abrandre@gmail.com>
2013-07-06 08:05:28 +0000

    modified4 again  web.js

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 
     var fs = require('fs');
-    var buf = new buffer();
+    var buf = new Buffer();
     var txt = buf.toString(fs.readFileSync('index.html'));
     response.send(buf.toString(fs.readFileSync('index.html')));
 
